7a9b256cd267f5443ec3766378a9ae3c6113c3c7
jahirxtrap <jahirroman2002@gmail.com>
2024-11-12 19:23:14 -0500

    Enchantment registry logic

--- neoforge/src/main/java/com/jahirtrap/walljump/init/ModEnchantments.java
+++ neoforge/src/main/java/com/jahirtrap/walljump/init/ModEnchantments.java
@@ -6,17 +6,22 @@ import com.jahirtrap.walljump.enchantment.WallJumpEnchantment;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
 
 import static com.jahirtrap.walljump.WallJumpMod.MODID;
 
 public class ModEnchantments {
     public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(Registries.ENCHANTMENT, MODID);
 
-    public static final DeferredHolder<Enchantment, WallJumpEnchantment> WALL_JUMP = ENCHANTMENTS.register("wall_jump", WallJumpEnchantment::new);
-    public static final DeferredHolder<Enchantment, DoubleJumpEnchantment> DOUBLE_JUMP = ENCHANTMENTS.register("double_jump", DoubleJumpEnchantment::new);
-    public static final DeferredHolder<Enchantment, SpeedBoostEnchantment> SPEED_BOOST = ENCHANTMENTS.register("speed_boost", SpeedBoostEnchantment::new);
+    public static final Supplier<Enchantment> WALL_JUMP = register("wall_jump", WallJumpEnchantment::new, ServerConfig.enableWallJump);
+    public static final Supplier<Enchantment> DOUBLE_JUMP = register("double_jump", DoubleJumpEnchantment::new, ServerConfig.enableDoubleJump);
+    public static final Supplier<Enchantment> SPEED_BOOST = register("speed_boost", SpeedBoostEnchantment::new, ServerConfig.enableSpeedBoost);
+
+    public static Supplier<Enchantment> register(String name, Supplier<Enchantment> supplier, boolean enable) {
+        return (ServerConfig.enableEnchantments && enable) ? ENCHANTMENTS.register(name, supplier) : null;
+    }
 
     public static void init(IEventBus bus) {
         ENCHANTMENTS.register(bus);
