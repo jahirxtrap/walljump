7f8afad5f4071a89622035fa03665420d0f3f43b
jahirxtrap <jahirroman2002@gmail.com>
2024-11-12 19:00:15 -0500

    Enchantment registry logic

--- fabric/src/main/java/com/jahirtrap/walljump/init/ModEnchantments.java
+++ fabric/src/main/java/com/jahirtrap/walljump/init/ModEnchantments.java
@@ -10,12 +10,12 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import static com.jahirtrap.walljump.WallJumpMod.MODID;
 
 public class ModEnchantments {
-    public static final Enchantment WALL_JUMP = register("wall_jump", new WallJumpEnchantment());
-    public static final Enchantment DOUBLE_JUMP = register("double_jump", new DoubleJumpEnchantment());
-    public static final Enchantment SPEED_BOOST = register("speed_boost", new SpeedBoostEnchantment());
+    public static final Enchantment WALL_JUMP = register("wall_jump", new WallJumpEnchantment(), ServerConfig.enableWallJump);
+    public static final Enchantment DOUBLE_JUMP = register("double_jump", new DoubleJumpEnchantment(), ServerConfig.enableDoubleJump);
+    public static final Enchantment SPEED_BOOST = register("speed_boost", new SpeedBoostEnchantment(), ServerConfig.enableSpeedBoost);
 
-    public static Enchantment register(String name, Enchantment enchantment) {
-        return Registry.register(Registry.ENCHANTMENT, new ResourceLocation(MODID, name), enchantment);
+    public static Enchantment register(String name, Enchantment enchantment, boolean enable) {
+        return (ServerConfig.enableEnchantments && enable) ? Registry.register(Registry.ENCHANTMENT, new ResourceLocation(MODID, name), enchantment) : null;
     }
 
     public static void init() {
