4cd3d5310a28e1b5aff45a7c31876eb14120e778
jahirxtrap <jahirroman2002@gmail.com>
2024-11-19 16:06:45 -0500

    Falling sound

--- fabric/src/main/java/com/jahirtrap/walljump/WallJumpClient.java
+++ fabric/src/main/java/com/jahirtrap/walljump/WallJumpClient.java
@@ -1,16 +1,22 @@
 package com.jahirtrap.walljump;
 
 import com.jahirtrap.keybindfix.KeybindFixer;
+import com.jahirtrap.walljump.init.ModConfig;
 import com.jahirtrap.walljump.network.PacketHandler;
+import com.jahirtrap.walljump.sound.FallingSoundInstance;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class WallJumpClient implements ClientModInitializer {
+    public static FallingSoundInstance FALLING_SOUND;
+
     public static KeyMapping KEY_WALL_JUMP = new KeyMapping(
             "key.walljump.walljump",
             GLFW.GLFW_KEY_LEFT_SHIFT,
@@ -22,5 +28,12 @@ public class WallJumpClient implements ClientModInitializer {
         KeyBindingHelper.registerKeyBinding(KEY_WALL_JUMP);
         KeybindFixer.putKey(KEY_WALL_JUMP);
         PacketHandler.initClient();
+    }
+
+    public static void playFallingSound(LocalPlayer player) {
+        if (ModConfig.playFallingSound && player.fallDistance > 1.5 && !player.isFallFlying() && (FALLING_SOUND == null || FALLING_SOUND.isStopped())) {
+            FALLING_SOUND = new FallingSoundInstance(player);
+            Minecraft.getInstance().getSoundManager().play(FALLING_SOUND);
+        }
     }
 }
