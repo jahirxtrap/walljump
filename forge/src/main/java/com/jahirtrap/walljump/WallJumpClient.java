8b57ad8c5e7ef45148ec1d072da9995aa1614140
jahirxtrap <jahirroman2002@gmail.com>
2024-11-19 16:08:11 -0500

    Falling sound

--- forge/src/main/java/com/jahirtrap/walljump/WallJumpClient.java
+++ forge/src/main/java/com/jahirtrap/walljump/WallJumpClient.java
@@ -1,6 +1,10 @@
 package com.jahirtrap.walljump;
 
+import com.jahirtrap.walljump.init.ModConfig;
+import com.jahirtrap.walljump.sound.FallingSoundInstance;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -9,6 +13,8 @@ import org.lwjgl.glfw.GLFW;
 
 @Mod.EventBusSubscriber(modid = WallJumpMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class WallJumpClient {
+    public static FallingSoundInstance FALLING_SOUND;
+
     public static KeyMapping KEY_WALL_JUMP = new KeyMapping(
             "key.walljump.walljump",
             GLFW.GLFW_KEY_LEFT_SHIFT,
@@ -18,5 +24,12 @@ public class WallJumpClient {
     @SubscribeEvent
     public static void registerKeyBinding(RegisterKeyMappingsEvent event) {
         event.register(KEY_WALL_JUMP);
+    }
+
+    public static void playFallingSound(LocalPlayer player) {
+        if (ModConfig.playFallingSound && player.fallDistance > 1.5 && !player.isFallFlying() && (FALLING_SOUND == null || FALLING_SOUND.isStopped())) {
+            FALLING_SOUND = new FallingSoundInstance(player);
+            Minecraft.getInstance().getSoundManager().play(FALLING_SOUND);
+        }
     }
 }
